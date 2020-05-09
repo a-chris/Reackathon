c0fe206da033518ed5e27a45f62aa85189cc12b5
JadeMugs <giada.boccali@gmail.com>
2020-05-09 10:46:14 +0200

    Added login inputs

--- client/src/user/Login.tsx
+++ client/src/user/Login.tsx
@@ -1,18 +1,46 @@
-import React from 'react';
-import { Box, Stack, Input } from '@chakra-ui/core';
+import React, { useState, ChangeEvent, useEffect } from 'react';
+import {
+    Box,
+    Stack,
+    Input,
+    InputRightElement,
+    Button,
+    InputGroup,
+    InputLeftElement,
+    Icon,
+    FormControl,
+    FormErrorMessage,
+} from '@chakra-ui/core';
 
-export default function Login() {
+type ChangeElem = ChangeEvent<HTMLInputElement>;
+
+export default function Login(): JSX.Element {
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [pwdVisible, setPwdVisible] = useState<boolean>(false);
+    const [pwdError, setError] = useState<boolean>(false);
+
+    const setValue = (event: ChangeElem, callback: Function) => {
+        const value = event.target.value;
+        callback(value);
+    };
+
+    useEffect(() => {
+        setError(false);
+    }, [password]);
+
+    const validatePwd = () => {
+        if (password.length > 0 && password.length < 6) {
+            setError(true);
+        }
+    };
+
+    function togglePwdVisibility() {
+        setPwdVisible(!pwdVisible);
+    }
+
     return (
-        <Stack
-            spacing={'-70px'}
-            // p={100}
-            width={[
-                '90%',
-                '80%',
-                '50%',
-                '40%',
-            ]}
-            m='auto'>
+        <Stack spacing={'-70px'} width={['90%', '80%', '50%', '40%']} m='auto'>
             <Box
                 overflow='visible'
                 borderWidth='1px'
@@ -22,15 +50,48 @@ export default function Login() {
                 m='auto'
                 bg='#e91e63'
                 zIndex={1}>
-                box sopra
+                Login
             </Box>
-            <Box overflow='hidden' borderWidth='1px' rounded='lg' pt='70px'>
+            <Box overflow='hidden' borderWidth='1px' rounded='md' pt='70px'>
                 <Stack spacing={3} p={8}>
-                    <Input variant='flushed' placeholder='Username' />
-                    <Input variant='flushed' placeholder='Nome e cognome' />
-                    {/* <InputLeftElement children={<Icon name="phone" color="gray.300" />} /> */}
-                    <Input type='phone' variant='flushed' placeholder='Phone number' />
+                    <FormControl>
+                        <InputGroup size='md'>
+                            <InputLeftElement children={<Icon name='spinner' />} />
+                            <Input
+                                type='text'
+                                placeholder='Username'
+                                value={username}
+                                variant='flushed'
+                                onChange={(e: ChangeElem) => setValue(e, setUsername)}
+                            />
+                        </InputGroup>
+                    </FormControl>
+                    <FormControl isInvalid={pwdError}>
+                        <InputGroup size='md'>
+                            <InputLeftElement children={<Icon name='lock' />} />
+                            <Input
+                                type={pwdVisible ? 'text' : 'password'}
+                                placeholder='Password'
+                                value={password}
+                                pr='4.5rem'
+                                variant='flushed'
+                                onChange={(e: ChangeElem) => setValue(e, setPassword)}
+                                onBlur={validatePwd}
+                            />
+                            <InputRightElement width='4.5rem'>
+                                <Button h='1.75rem' size='sm' onClick={togglePwdVisibility}>
+                                    {pwdVisible ? 'Nascondi' : 'Mostra'}
+                                </Button>
+                            </InputRightElement>
+                        </InputGroup>
+                        <FormErrorMessage>
+                            La password deve essere lunga almeno 6 caratteri
+                        </FormErrorMessage>
+                    </FormControl>
                 </Stack>
+                <Button variantColor='teal' isLoading={false} type='submit'>
+                    Login
+                </Button>
             </Box>
         </Stack>
     );
