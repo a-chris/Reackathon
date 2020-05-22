aaad030a76d88a708ed2a2a415fb08c91f771d0d
JadeMugs <giada.boccali@gmail.com>
2020-05-22 13:51:53 +0200

    add hackathons in hackathon list

--- client/src/pages/hackathons/HackathonsList.tsx
+++ client/src/pages/hackathons/HackathonsList.tsx
@@ -1,9 +1,21 @@
+import { Box, Text } from '@chakra-ui/core';
 import React from 'react';
+import { Hackathon } from '../../models/Models';
+import { getHackathons } from '../../services/HackathonService';
 
 export default function HackathonsList() {
+    const [hackathons, setHackathons] = React.useState<Hackathon[]>([]);
+
+    React.useEffect(() => {
+        getHackathons().then((hackathons) => setHackathons(hackathons));
+    }, []);
+
     return (
-        <div>
-            <p>hackathons</p>
-        </div>
+        <Box p={15}>
+            <Box w='30%' color='red.500'>
+                <Text as='h2'>Hackathons</Text>
+            </Box>
+            <Box w='70%' color='gray.500'></Box>
+        </Box>
     );
 }
--- client/src/services/HackathonService.ts
+++ client/src/services/HackathonService.ts
@@ -1,6 +1,16 @@
 import axios from 'axios';
 import { Hackathon } from '../models/Models';
 
+export function getHackathons(): Promise<Hackathon[]> {
+    // TODO: add params
+    return new Promise((resolve, reject) =>
+        axios
+            .get('http://localhost:5000/hackathons')
+            .then((response: any) => resolve(response.data))
+            .catch((error: any) => reject(error))
+    );
+}
+
 export function creation(hackathonData: Hackathon): Promise<Hackathon> {
     return new Promise((resolve, reject) =>
         axios
