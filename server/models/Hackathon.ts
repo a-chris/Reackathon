f8d59c148b497512953d2777e5f2959e55a14e62
JadeMugs <giada.boccali@gmail.com>
2020-05-29 22:55:25 +0200

    update location schema

--- server/models/Hackathon.ts
+++ server/models/Hackathon.ts
@@ -1,30 +1,46 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose from 'mongoose';
 import { User } from './User';
 
 const LocationSchema = new mongoose.Schema({
+    street: String,
+    number: Number,
+    city: String,
+    province: String,
+    district: String,
+    country: String,
+    zip_code: Number,
     lat: Number,
     long: Number,
 });
 
 const PrizeSchema = new mongoose.Schema({
-    amount: Number,
+    amount: {
+        type: Number,
+        min: 0,
+    },
     extra: String,
 });
 
 const HackathonSchema = new mongoose.Schema({
     name: String,
-    organization: { type: Schema.Types.ObjectId, ref: 'User' },
-    attendants: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    organization: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    attendants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     startDate: Date,
     endDate: Date,
-    status: { type: String, enum: ['started', 'finished'] },
-    province: String,
-    coords: LocationSchema,
+    status: { type: String, enum: ['pending', 'started', 'finished'] },
+    location: LocationSchema,
     description: String,
     prize: PrizeSchema,
 });
 
 export type Location = mongoose.Document & {
+    street: string;
+    number: number;
+    city: string;
+    province: string;
+    district: string;
+    country: string;
+    zip_code: number;
     lat: number;
     long: number;
 };
@@ -42,8 +58,7 @@ export type Hackathon = mongoose.Document & {
     startDate: Date;
     endDate: Date;
     status: string;
-    province: string;
-    coords: Location;
+    location: Location;
     prize: Prize;
 };
 
