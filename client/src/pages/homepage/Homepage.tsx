685ef8ba27a167a5f572d7a00678b78b48b62488
JadeMugs <giada.boccali@gmail.com>
2020-06-03 00:57:07 +0200

    adds basic homepage

--- client/src/pages/homepage/Homepage.tsx
+++ client/src/pages/homepage/Homepage.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { MapContainer } from '../../components/Map';
 import { getHackathons } from '../../services/HackathonService';
 import { Hackathon } from '../../models/Models';
-import { Stack, PseudoBox, Grid, Box, Heading } from '@chakra-ui/core';
+import { Stack, PseudoBox, Grid, Box, Heading, Image } from '@chakra-ui/core';
 import { Link } from 'react-router-dom';
+import { yellow } from '../../utils/colors';
 
 const initialFilter = { country: 'Italy' };
 
@@ -19,29 +20,47 @@ export default function Homepage() {
     }, []);
 
     return (
-        <PseudoBox w={'100%'} h={'100%'}>
-            <Stack w={'100%'} h={'100%'} isInline spacing={8} bg='#000000'>
+        <Box w={'100%'} h={'100%'}>
+            <Box h={'100%'} bg={yellow}>
+                {cities.size > 0 && (
+                    <Box p={8}>
+                        <Heading as='h2' p={10}>
+                            Scopri le città con i prossimi Hackathon
+                        </Heading>
+                        <Grid templateColumns={'repeat(4, 1fr)'} gap={6} alignItems='center'>
+                            {[...cities]?.map((city, index) => (
+                                <Link to={'hackathons?city=' + city} key={index}>
+                                    <Box w='100%' bg='blue.200' verticalAlign='middle'>
+                                        <Image
+                                            src={'./images/cities/' + city.toLowerCase() + '.jpg'}
+                                            fallbackSrc='./images/cities/city.jpg'
+                                            alt={'immagine della città di ' + city}
+                                        />
+                                        <Heading as='h3'>{city}</Heading>
+                                        {console.log(
+                                            "url('./images/cities/" + city.toLowerCase() + ".jpg')"
+                                        )}
+                                    </Box>
+                                </Link>
+                            ))}
+                        </Grid>
+                    </Box>
+                )}
+            </Box>
+            <Box w={'100%'} h={'100%'} display={{ md: 'flex' }}>
                 <MapContainer hackathons={hackathons} />
                 <Stack>
                     <PseudoBox>Trova l'Hackathon più adatto a te!</PseudoBox>
                 </Stack>
-            </Stack>
-            {cities.size > 0 && (
-                <PseudoBox p={8}>
-                    <Heading as='h2' p={5}>
-                        Scopri le città con i prossimi Hackathon
-                    </Heading>
-                    <Grid templateColumns={'repeat(' + cities.size + ', 1fr)'} gap={6}>
-                        {[...cities]?.map((city, index) => (
-                            <Link to={'hackathons?city=' + city} key={index}>
-                                <Box w='20%' h='10' bg='blue.500' verticalAlign='middle'>
-                                    <p>{city}</p>
-                                </Box>
-                            </Link>
-                        ))}
-                    </Grid>
-                </PseudoBox>
-            )}
-        </PseudoBox>
+            </Box>
+            <Box>
+                <Heading as='h3' p={10}>
+                    Stai organizzando un Hackathon?
+                </Heading>
+                <Heading as='h4'>
+                    Registrati sulla piattaforma e dai visibilità al tuo Hackathon
+                </Heading>
+            </Box>
+        </Box>
     );
 }
